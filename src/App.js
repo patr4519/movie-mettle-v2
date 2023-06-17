3db8f8b835ca68f3d662f26e87322e43b9d08a32
patr4519 <jero96@mail.ru>
2023-06-17 13:33:47 +0300

    +

--- src/App.js
+++ src/App.js
@@ -4,22 +4,52 @@ import React from "react";
 import MovieSearch from "./MovieSearch";
 import { Film } from "./features/Film";
 import Header from "./features/Header";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
-  const bestFilms = ["The Godfather", "The Dark Knight", "Fight Club", "The Godfather", "The Dark Knight", "Fight Club", "The Godfather", "The Dark Knight", "Fight Club", "The Godfather"];
+  const bestFilms = [
+    "The Godfather",
+    "The Dark Knight",
+    "Fight Club",
+    "The Godfather",
+    "The Dark Knight",
+    "Fight Club",
+    "The Godfather",
+    "The Dark Knight",
+    "Fight Club",
+    "The Godfather",
+  ];
 
   return (
-    <div className="wrapper-app">
-      <div className="app">
-        <Header />
-        <MovieSearch />
-        <div className="movie-list">
-          {bestFilms.map((title, index) => {
-            return <Film key={index} title={title} />;
-          })}
+    <Router>
+      <div className="wrapper-app">
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <MovieSearch />
+                  <div className="movie-list">
+                    {bestFilms.map((title, index) => {
+                      return <Film key={index} title={title} />;
+                    })}
+                  </div>
+                </>
+              }
+            />
+            <Route path="/favorites" element={<div>favorites</div>} />
+          </Routes>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/features/Header.jsx
+++ src/features/Header.jsx
@@ -12,6 +12,8 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import { Link } from 'react-router-dom';
+
 
 const pages = ['Home', 'Favorites', 'Forum'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
