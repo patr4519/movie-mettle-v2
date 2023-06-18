ff17e178c232a233deaa32d51e54d111c21f4c58
patr4519 <jero96@mail.ru>
2023-06-18 21:49:03 +0300

    +

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from '../features/api/apiSlice'
+import favoritesReducer from '../features/api/favoritesSlice'
 
 export default configureStore({
   reducer: {
+    favorites: favoritesReducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
   middleware: getDefaultMiddleware =>
---
+++ src/features/api/favoritesSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const favoritesSlice = createSlice({
+  name: "favorites",
+  initialState,
+  reducers: {
+    addFav: (state, action) => {
+      state.items.push(action.payload);
+    },
+  },
+});
+
+export const selectFavorites = (state) => state.favorites;
+
+export const { addFav } = favoritesSlice.actions;
+
+export default favoritesSlice.reducer;
