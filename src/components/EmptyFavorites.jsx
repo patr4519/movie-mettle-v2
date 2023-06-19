ec930cdee9fb4cd56a44c488a860969017fc7bcd
patr4519 <jero96@mail.ru>
2023-06-19 12:27:52 +0300

    +

---
+++ src/components/EmptyFavorites.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; // Import the CSS file for styling
+
+const EmptyFavorites = () => {
+  return (
+    <div className="empty-favorites">
+      <h2>No favorite films</h2>
+      <p>Add some films to your favorites list!</p>
+    </div>
+  );
+};
+
+export default EmptyFavorites;
--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectFavorites } from "../features/api/favoritesSlice";
 import { FavFilm } from "./FavFilm";
+import EmptyFavorites from "./EmptyFavorites";
 
 export const Favorites = () => {
   const favorites = useSelector(selectFavorites);
+
+  if (!favorites.length) return <EmptyFavorites />
 
   return (
     <div className="movie-list">
