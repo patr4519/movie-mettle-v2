69a491f847ee98d1114c2bcb707589723dfbb9b4
patr4519 <jero96@mail.ru>
2023-06-26 19:50:58 +0300

    last commit before TypeScript

--- src/components/AuthorizationBlock.jsx
+++ src/components/AuthorizationBlock.jsx
@@ -3,16 +3,17 @@ import { LogIn } from "./LogIn";
 import { SignUp } from "./SignUp";
 
 export const AuthorizationForm = () => {
-  const [toggle, setToggle] = React.useState('up');
+  const [toggle, setToggle] = React.useState("up");
 
   return (
     <div className="autorize-form">
       <div>Join MovieMettle</div>
       <h2>First, let's create your user account</h2>
-      {toggle === 'in' && <LogIn />} 
-      {toggle === 'up' && <SignUp />}
+      {toggle === "in" && <LogIn />}
+      {toggle === "up" && <SignUp />}
       <div>
-        Already have an account? <span onClick={() => setToggle('in')}>Sign in</span>
+        Already have an account?{" "}
+        <span onClick={() => setToggle("in")}>Sign in</span>
       </div>
     </div>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { SignUp } from "./SignUp";
-import { LogIn } from "./LogIn";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/api/userSlice";
 import UserProfile from "./UserProfile";
